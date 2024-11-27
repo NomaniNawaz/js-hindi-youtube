6d18e007e2acdd08a6891c2f1f9d21e044122ed9
NomaniNawaz <162862174+NomaniNawaz@users.noreply.github.com>
2024-11-27 12:34:26 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // trat all js code as newer version 
+
+// alert(3+3) we are using nodejs, not brouser "java script ka ju engine hain wo brouser ke ander chippa hai"
+ 
+console.log(3+3) //  code readable huna cahea aaisa nhi ki 
+// agar code chalne laga tu sahi hai usse padha bhii ja sake
+console.log("Arsalan");
+
+
+// some types of data types
+
+let name = "Arsalan"
+let age = 20
+let isloggedIn = true/false 
+
+// number => 2 to ki power 53
+// bigint (agar number bahut zyada bada hai tu isse use krte hai )
+// string =>""
+// boolean =>true/false
+// null => standalone value "specially type hai ye"
+// undefined => means issme kuch define nhi kiya gya hai
+// symbol => unique
+
+// object
+
+// console.log(typeof null); ye object hai
+// console.log(typeof undefined); ye undefined
